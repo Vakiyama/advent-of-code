43b7b3e553314979d71e51873c889084861a2614
Vakiyama <vakiyama@outlook.com>
2023-12-06 18:00:42 -0800

    add flake for nixos

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+    description = "a Bun ts project";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem ( system:
+        let
+            pkgs = nixpkgs.legacyPackages.${ system };
+        in {
+            devShell = with pkgs; pkgs.mkShell rec {
+                buildInputs = [
+                    bun
+                    nodejs_20
+                ];
+
+                shellHook = ''
+                    # set local home
+                    LOCAL_HOME=$PWD/.home
+                    mkdir -p $LOCAL_HOME
+                    mkdir -p $LOCAL_HOME/.config
+                    
+                    # symlink nvim and starship 
+                    if [ ! -L "$PWD/.home/.config/starship.toml" ]; then
+                        ln -s $HOME/.config/starship.toml $LOCAL_HOME/.config/starship.toml
+                    fi
+
+                    export HOME=$LOCAL_HOME
+                '';
+            };
+        }
+    );
+}
